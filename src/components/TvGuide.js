42b69f9c4a9c2bc91af51bdb9ba2cda7bc142090
Amalie Smidth <amaliesmidth@gmail.com>
2019-02-20 15:35:37 +0000

    created components

---
+++ src/components/TvGuide.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class TvGuide extends Component {
+
+    state = {
+
+    }
+
+    render() {
+        return (
+            <div></div>
+        );
+    }
+}
+
+export default TvGuide
