52ed0fa4730e852d3adf0215c723b09604a41af5
Amalie Smidth <amaliesmidth@gmail.com>
2019-02-21 12:38:29 +0000

    draft of sorting data function

--- src/components/TvGuide.js
+++ src/components/TvGuide.js
@@ -5,12 +5,13 @@ import ChannelRowsContainer from './ChannelRowsContainer';
 class TvGuide extends Component {
 
     state = {
-
+        channelProgrammes: {}
     }
 
-    componentDidMount() {
+    async componentDidMount() {
         const fileNames = ["1", "2", "3", "4", "5", "6", "channels"]
-        fileNames.forEach( file => this.getData(file))
+        await fileNames.forEach( file => this.getData(file))
+            .then(this.sortData())
     }
 
     getData = async (fileName) => {
@@ -21,9 +22,47 @@ class TvGuide extends Component {
         this.setState({[key]: extractedData})
     }
 
+    sortData = () => {
+        const days = [this.state["1"], this.state["2"], this.state["3"], this.state["4"], this.state["5"], this.state["6"]]
+        // console.log(days)
+        days.forEach(day => {
+            // console.log(day)
+            day.forEach(programme => {
+                if (this.state.channelProgrammes.hasOwnProperty(programme.channelID)) {
+                    const key = `channelProgrammes.${programme.channelID}`
+                    const programmes = [...this.state.channelProgrammes.channelID]
+                    programmes.push(programme)
+                    this.setState({ [key]: programmes })
+                } else {
+                    const key = `channelProgrammes.${programme.channelID}`
+                    const starterArray = [...programme]
+                    this.setState({ [key]: starterArray })
+                }
+            }
+            )
+        }
+        )
+    }
+
+    // sortData = () => {
+    //     const fileNames = ["1", "2", "3", "4", "5", "6"]
+    //     fileNames.forEach(file =>
+    //         file.map(programme => 
+    //             this.state.channelProgrammes.hasOwnProperty(programme.channelID) ?
+
+    //                 (const key = `channelProgrammes.${programme.channelID}`
+    //             const programmes = [...this.state.channelProgrammes.channelID]
+    //             programmes.push(programme)
+    //             this.setState({ [key]: programmes })) :
+
+    //         (const key = `channelProgrammes.${programme.channelID}`
+    //             const starterArray = [...programme]
+    //             this.setState({ [key]: starterArray }))  
+    //         )
+    //     )
+    // }
+
     render() {
-        // const programmes = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K"]
-        // const channel = 'BBC'
         const { channels } = this.state
 
         return (
